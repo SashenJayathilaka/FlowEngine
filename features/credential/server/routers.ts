b6774ee67a55ef10c35674945c2a455939e193c9
SashenJayathilaka <sashenhasindu@gmail.com>
2025-11-14 13:13:47 +1100

    🗝️ Credentials Form

---
+++ features/credential/server/routers.ts
@@ -0,0 +1,132 @@
+import { PAGINATION } from "@/config/constants";
+import prisma from "@/lib/db";
+import { CredentialType } from "@/lib/generated/prisma";
+import {
+  createTRPCRouter,
+  premiumProcedure,
+  protectedProcedure,
+} from "@/trpc/init";
+import z from "zod";
+
+export const credentialsRouter = createTRPCRouter({
+  create: premiumProcedure
+    .input(
+      z.object({
+        name: z.string().min(1, "Credential name is required"),
+        type: z.enum(CredentialType),
+        value: z.string().min(1, "Credential value is required"),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      const { name, type, value } = input;
+
+      return prisma.credential.create({
+        data: {
+          name,
+          type,
+          value, // TODO: Encrypt value before storing
+          userId: ctx.auth.user.id,
+        },
+      });
+    }),
+  remove: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return prisma.credential.delete({
+        where: {
+          id: input.id,
+          userId: ctx.auth.user.id,
+        },
+      });
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string().min(1, "Credential name is required"),
+        value: z.string().min(1, "Credential value is required"),
+        type: z.enum(CredentialType),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, name, value, type } = input;
+
+      return prisma.credential.update({
+        where: { id, userId: ctx.auth.user.id },
+        data: {
+          name,
+          value, // TODO: Encrypt value before storing
+          type,
+        },
+      });
+    }),
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return prisma.credential.findUniqueOrThrow({
+        where: { id: input.id, userId: ctx.auth.user.id },
+      });
+    }),
+
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        page: z.number().default(PAGINATION.DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(PAGINATION.MIN_PAGE_SIZE)
+          .max(PAGINATION.MAX_PAGE_SIZE)
+          .default(PAGINATION.DEFAULT_PAGE_SIZE),
+        search: z.string().default(""),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { page, pageSize, search } = input;
+
+      const [items, totalCount] = await Promise.all([
+        prisma.credential.findMany({
+          skip: (page - 1) * pageSize,
+          take: pageSize,
+
+          where: {
+            userId: ctx.auth.user.id,
+            name: { contains: search, mode: "insensitive" },
+          },
+          orderBy: { updatedAt: "desc" },
+          /*   select: {
+            id: true,
+            name: true,
+            type: true,
+            createdAt: true,
+            updatedAt: true,
+          }, */
+        }),
+        prisma.credential.count({ where: { userId: ctx.auth.user.id } }),
+      ]);
+
+      const totalPage = Math.ceil(totalCount / pageSize);
+      const hasNextPage = page < totalPage;
+      const hasPreviousPage = page > 1;
+
+      return {
+        items,
+        totalCount,
+        totalPage,
+        hasNextPage,
+        hasPreviousPage,
+        currentPage: page,
+        pageSize,
+      };
+    }),
+
+  getByType: protectedProcedure
+    .input(z.object({ type: z.enum(CredentialType) }))
+    .query(({ ctx, input }) => {
+      const { type } = input;
+
+      return prisma.credential.findMany({
+        where: { type, userId: ctx.auth.user.id },
+        orderBy: { updatedAt: "desc" },
+      });
+    }),
+});
--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,8 +1,10 @@
+import { credentialsRouter } from "@/features/credential/server/routers";
 import { workflowsRouters } from "@/features/workflows/server/routers";
 import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
   workflows: workflowsRouters,
+  credentials: credentialsRouter,
 });
 
 export type AppRouter = typeof appRouter;
